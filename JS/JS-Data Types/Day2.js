de8131ea2fe01b477928e342b36747ddf77ada40
prafullarawat <prafullasrawat@gmail.com>
2022-07-13 12:17:16 +0530

    Create Day2.js

---
+++ JS/JS-Data Types/Day2.js
@@ -0,0 +1,31 @@
+//Primitive Data Types
+
+let name = "Prafulla";
+console.log("My String Is " + name);
+console.log("Data Type Is " + typeof name);
+
+let mark = 85;
+console.log("Data Type Is " + typeof marks);
+
+let isDriver = true;
+console.log("Data Type Is " + typeof isDriver);
+
+let nullVar = null;
+console.log("Data Type Is " + typeof nullVar);
+
+//Reference Data Types
+
+let myarr = [1, 3, 5, 6, 7];
+console.log("Data Type Is " + typeof myarr);
+
+//Object Literals
+
+let marksp = {
+  Prafulla: 80,
+  Rawat: 78,
+  Suresh: 70,
+};
+console.log(marksp);
+
+function findName() {}
+console.log("Data Type Is " + typeof findName);

ae495184e88604c63c495643d153b187b0674078
prafullarawat <prafullasrawat@gmail.com>
2022-07-14 18:22:19 +0530

    Create Day2.js

---
+++ JS/JS-Conversion & Coercion/Day2.js
@@ -0,0 +1,9 @@
+console.log("Conversion & Coercion");
+
+let myNo;
+
+myNo = String(78);
+console.log(myNo, typeof myNo);
+
+let arr = String([1, 2, 4, 5, 6, 7, 9]);
+console.log(arr.length, typeof arr); //Length=> For arr 7 For String 13

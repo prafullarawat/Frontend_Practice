518190688ba04bf4dc032d43aa4580ac66fe3826
prafullarawat <prafullasrawat@gmail.com>
2022-07-18 08:52:01 +0530

    Create Day3.js

---
+++ JS/JS-Loops/Day3.js
@@ -0,0 +1,19 @@
+//For_Loop
+for (let i = 0; i <= 100; i++) {
+  console.log(i);
+}
+
+//While_Loop
+let j = 1;
+while (j <= 10) {
+  console.log(j);
+  j = j + 1;
+}
+
+//Do_While
+let k = 50;
+let n = 60;
+do {
+  console.log(k);
+  k++;
+} while (k <= n);

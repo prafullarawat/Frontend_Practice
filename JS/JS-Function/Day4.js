2d64c0156b086b7f3882fd436a0cdc181d7dc04a
prafullarawat <prafullasrawat@gmail.com>
2022-07-20 22:07:05 +0530

    Create Day4.js

---
+++ JS/JS-Function/Day4.js
@@ -0,0 +1,9 @@
+function pract(name) {
+  console.log(
+    `I am ${name}. I have completed my graduation in Computer Engineering. After that I have completed my PG-Diploma from CDAC. Currently I am working in BORN Group as FrontEnd Developer`
+  );
+}
+
+let name = "Prafulla";
+
+pract(name);

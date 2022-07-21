0a22fd09e3539d1a001fb2bf15fbc31e8766e089
prafullarawat <prafullasrawat@gmail.com>
2022-07-21 12:30:03 +0530

    Update Day4.js

--- JS/JS-Function/Day4.js
+++ JS/JS-Function/Day4.js
@@ -1,9 +1,11 @@
-function pract(name) {
+//Function Creation
+function pract(name, name1) {
   console.log(
-    `I am ${name}. I have completed my graduation in Computer Engineering. After that I have completed my PG-Diploma from CDAC. Currently I am working in BORN Group as FrontEnd Developer`
+    `I am ${name}. I have completed my graduation in Computer Engineering. After that I have completed my PG-Diploma from CDAC. 
+    Currently I am working in BORN Group as FrontEnd Developer ${name1} `
   );
 }
 
 let name = "Prafulla";
 
-pract(name);
+pract(name, "Hello World"); //Function Call

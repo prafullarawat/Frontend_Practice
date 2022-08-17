28deda7536bc33aa01154f34fb2032324500905b
prafullarawat <prafullasrawat@gmail.com>
2022-08-17 11:01:06 +0530

    Update Day2.js

--- JS/JS-Data Types/Day2.js
+++ JS/JS-Data Types/Day2.js
@@ -4,7 +4,7 @@ let name = "Prafulla";
 console.log("My String Is " + name);
 console.log("Data Type Is " + typeof name);
 
-let mark = 80;
+let mark = 90;
 console.log("Data Type Is " + typeof marks);
 
 let isDriver = true;

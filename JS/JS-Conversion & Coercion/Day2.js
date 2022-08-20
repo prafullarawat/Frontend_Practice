4c5e2de4c008fccdf63668d552b135afdc3a0940
prafullarawat <prafullasrawat@gmail.com>
2022-08-20 21:59:21 +0530

    Update Day2.js

--- JS/JS-Conversion & Coercion/Day2.js
+++ JS/JS-Conversion & Coercion/Day2.js
@@ -2,8 +2,8 @@ console.log("Conversion & Coercion");
 
 let myNo;
 
-myNo = String(78);
+myNo = String(98);
 console.log(myNo, typeof myNo);
 
-let arr = String([1, 2, 4, 5, 6, 7, 9]);
+let arr = String([1, 3, 4, 5, 6, 8, 9]);
 console.log(arr.length, typeof arr); //Length=> For arr 7 For String 13

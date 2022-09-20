fb95b8633607469a00cc6106129b25e9cd05ea5a
prafullarawat <prafullasrawat@gmail.com>
2022-09-20 20:13:12 +0530

    Update Day5.js

--- JS/JS-Selectors/Day5.js
+++ JS/JS-Selectors/Day5.js
@@ -1,5 +1,5 @@
 //1. SingleElementSelector.
 //2. MultiElementSelector.
 
-let elem = document.getElementsByClassName("container");
-console.log(elem);
+let ele = document.getElementsByClassName("container");
+console.log(ele);

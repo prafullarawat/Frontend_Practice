78cee4ac6f5ce0017921418fa741d77361a04f6f
prafullarawat <prafullasrawat@gmail.com>
2022-10-11 20:38:56 +0530

    Update Day3.js

--- JS/JS-Loops/Day3.js
+++ JS/JS-Loops/Day3.js
@@ -1,11 +1,11 @@
 //For_Loop
-for (let c = 0; c <= 100; c++) {
+for (let c = 1; c <= 10; c++) {
   console.log(c);
 }
 
 //While_Loop
-let l = 11;
-while (l <= 100) {
+let l = 1;
+while (l <= 10) {
   console.log(s);
   l = l + 1;
 }

098e3665bc9630f8eb0788a55e900607e11530ff
prafullarawat <prafullasrawat@gmail.com>
2022-11-01 22:12:59 +0530

    Update Day2.js

--- JS/JS-Data Types/Day2.js
+++ JS/JS-Data Types/Day2.js
@@ -1,10 +1,10 @@
 //Primitive_Data_Types
 
-let name = "Praf";
+let name = "Pra";
 console.log("My_String_Is " + name);
 console.log("Data_Type_Is " + typeof name);
 
-let mark = 95;
+let mark = 9;
 console.log("Data_Type_Is " + typeof marks);
 
 let isDriver = false;
@@ -15,7 +15,7 @@ console.log("Data Type Is " + typeof nullVar);
 
 //Reference Data Types
 
-let myarr = [1, 3, 5, 6, 7];
+let myarr = [1, 5, 6, 7];
 console.log("Data Type Is " + typeof myarr);
 
 //Object Literals
